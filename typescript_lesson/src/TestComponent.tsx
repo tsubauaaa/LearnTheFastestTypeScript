fb15f0acb1f554c7ba867e1edc71223788720d61
ts-hirota <ts-hirota@kddi.com>
2021-05-28 22:37:02 +0900

    done

--- typescript_lesson/src/TestComponent.tsx
+++ typescript_lesson/src/TestComponent.tsx
@@ -1,13 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
 interface Props {
   text: string;
 }
 
+interface UserData {
+  id: number;
+  name: string;
+}
+
 const TestComponent: React.FC<Props> = (props) => {
+  const [count, setCount] = useState<number | null>(0);
+  const [user, setUser] = useState<UserData>({id: 1, name: "hirota"});
+  const [inputData, setInputData] = useState("");
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => setInputData(e.target.value);
+
   return (
     <div>
       <h1>{props.text}</h1>
+      <h1>{count}</h1>
+      <input type="text" value={inputData} onChange={handleInputChange}/>
+      <h1>{inputData}</h1>
     </div>
   );
 };
